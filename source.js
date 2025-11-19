baeb8ddb37b05c12b1ea03e9b1b35668cf91baef
karianjahi <kitgaedesigaidi8@gmail.com>
2025-11-19 18:40:28 +0100

    Add user stories and initialize the function

---
+++ source.js
@@ -0,0 +1,16 @@
+/*
+Implement a Range-Based LCM Calculator
+In this lab, you will create a function that takes an array of two numbers and returns the least common multiple (LCM) of those two numbers and all the numbers between them.
+
+User Stories
+
+1 You should have a `smallestCommons` function that accepts an array of two numbers as an argument.
+2 The `smallestCommons` function should return the smallest common multiple that is evenly divisible by both numbers and all sequential numbers in the range between them.
+3 The function should handle input where the two numbers are not in numerical order.
+*/
+
+const smallestCommons = (arrayOf2Nos) => {
+    return arrayOf2Nos;
+}
+
+console.log(smallestCommons([1, 5]));
