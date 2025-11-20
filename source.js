48ff53fa80fa41ae8e115e4f6994e882ad1b001e
karianjahi <kitgaedesigaidi8@gmail.com>
2025-11-20 11:32:41 +0100

    Remove unnecessary comments

--- source.js
+++ source.js
@@ -25,15 +25,15 @@ const smallestCommons = (myArray) => {
   let rangeValues = [];
   let lcm = 1;
   for (let i = myArray[0]; i <= myArray[1]; i++) rangeValues.push(i);
-  console.log(`Range of values: ${rangeValues}`);
+//   console.log(`Range of values: ${rangeValues}`);
   for (let i = 1; i < rangeValues.length; i++) {
     if (i == 1) {
         lcm = getLCM(rangeValues[i-1], rangeValues[i]);
-        console.log(`lcm between ${rangeValues[i-1]} and ${rangeValues[i]} is ${lcm}`);
+        // console.log(`lcm between ${rangeValues[i-1]} and ${rangeValues[i]} is ${lcm}`);
     } else {
-        oldLcm = lcm
+        // oldLcm = lcm
         lcm = getLCM(lcm, rangeValues[i])
-         console.log(`lcm between ${oldLcm} and ${rangeValues[i]} is ${lcm}`);
+        //  console.log(`lcm between ${oldLcm} and ${rangeValues[i]} is ${lcm}`);
     }
   }
   return lcm;
